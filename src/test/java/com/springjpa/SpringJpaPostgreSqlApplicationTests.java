c05deb34058b251cfdfaf38bbdba9359c9931e91
Leo <Leo_Kuo@wistron.com>
2018-03-05 16:26:24 +0800

    Auto Creat Table

--- src/test/java/com/springjpa/SpringJpaPostgreSqlApplicationTests.java
+++ src/test/java/com/springjpa/SpringJpaPostgreSqlApplicationTests.java
@@ -1,14 +1,25 @@
 package com.springjpa;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
+
 
 //@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringJpaPostgreSqlApplicationTests {
+	public void testApp() {
+		SessionFactory sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
 
+		session.getTransaction().commit();
+		session.close();
+	}
 	@Test
 	public void contextLoads() {
 	}
